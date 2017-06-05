17beae21db7650485052987a523bb93b95393e3f
Marek Rogóz <marekrogoz@outlook.com>
2017-06-05 14:11:11 +0200

    fixed

--- scripts/dotnet-restore.sh
+++ scripts/dotnet-restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source="--source https://api.nuget.org/v3/index.json --source https://www.myget.org/F/photoalbum/api/v3/index.json --no-cache"
-#cd ../PhotoAlbum
+cd ../
 projects=(PhotoAlbum)
 for project in ${projects[*]}
 do
